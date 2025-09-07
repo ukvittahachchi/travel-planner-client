6099547eeb680f65385441fbcb2a6aa677ebb897
ukvittahachchi <ukvittahachchi99@gmail.com>
2025-09-07 12:38:32 +0530

    Ai driven planner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import ItineraryBuilder from "./pages/ItineraryBuilder";
-import CostEstimator from "./pages/CostEstimator";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import GroupPlanning from "./pages/GroupPlanning";
+
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -20,23 +18,9 @@ function App() {
                     <Dashboard />
                   </ProtectedRoute>
                 } />
-        <Route path="/itinerary-builder" element={
-                  <ProtectedRoute>
-                    <ItineraryBuilder />
-                  </ProtectedRoute>
-                } />
-        <Route path="/cost-estimator/:tripId" element={
-                  <ProtectedRoute>
-                    <CostEstimator />
-                  </ProtectedRoute>
-                } />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/group-planning/:tripId" element={
-                  <ProtectedRoute>
-                    <GroupPlanning />
-                  </ProtectedRoute>
-                } />
+        
       </Routes>
     </Router>
   );
